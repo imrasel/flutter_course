fcfbf52f820085e36ad1a91454ded788bd7a220f
Rasel Ahmed <ras3lahmed@gmail.com>
2019-07-05 17:30:05 +0600

    Add project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './pages/auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+        brightness: Brightness.light
+      ),
+      home: AuthPage()
+    );
+  }
+}
+
